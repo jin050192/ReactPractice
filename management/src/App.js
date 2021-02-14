180c3f43aed1dadc43349264da52806fa71e731f
jin050192 <jin050192@gmail.com>
2021-02-14 15:58:43 +0900

    반복문 이용하여 여러고객 출력(map이용)

--- management/src/App.js
+++ management/src/App.js
@@ -1,12 +1,53 @@
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
+import Customer from './components/Customer';
+
+const customers = [
+  {
+    'id' : 1,
+    'image' : 'https://placeimg.com/64/64/1', // placeimg 사이트는 이미지를 랜덤으로 가져옴. 보여주고싶은 이미지는 64x64크기
+    'name' : '진은성',
+    'birthday' : '910501',
+    'gender' : '남자',
+    'job' : 'Programmer'
+  },
+  {
+    'id' : 2,
+    'image' : 'https://placeimg.com/64/64/2', // placeimg 사이트는 이미지를 랜덤으로 가져옴. 보여주고싶은 이미지는 64x64크기
+    'name' : '진은성2',
+    'birthday' : '920501',
+    'gender' : '남자',
+    'job' : 'Programmer'
+  },
+  {
+    'id' : 3,
+    'image' : 'https://placeimg.com/64/64/3', // placeimg 사이트는 이미지를 랜덤으로 가져옴. 보여주고싶은 이미지는 64x64크기
+    'name' : '진은성3',
+    'birthday' : '930501',
+    'gender' : '남자',
+    'job' : 'Programmer'
+  }
+]
 
 function App() {
   return (
-    <div className="gray-background">
-      <img src={logo} lat="logo"></img>
-      <h2>Let's develop management system!</h2>
-    </div>
+    <div>
+      {
+        customers.map(c => {
+          return (
+            <Customer
+              key={c.id}
+              id={c.id}
+              image={c.image}
+              name={c.name}
+              birthday={c.birthday}
+              gender={c.gender}
+              job={c.job}
+            />
+          )
+        })
+      }
+  </div>
   );
 }
 
