8733cbfbf33e2e298eecc6a70afbce7cefba4a97
jin050192 <jin050192@gmail.com>
2021-02-16 20:34:31 +0900

    데이터베이스와 연결

--- management/server.js
+++ management/server.js
@@ -1,3 +1,4 @@
+const fs = require('fs');
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
@@ -6,33 +7,26 @@ const port = process.env.PORT || 5000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+const data = fs.readFileSync('./database.json');
+const conf = JSON.parse(data);
+const mysql = require('mysql');
+
+const connection = mysql.createConnection({
+  host: conf.host,
+  user: conf.user,
+  password: conf.password,
+  port: conf.port,
+  database: conf.database
+});
+connection.connect();
+
 app.get('/api/customers', (req, res) => {
-    res.send([
-        {
-            'id' : 1,
-            'image' : 'https://placeimg.com/64/64/1', // placeimg 사이트는 이미지를 랜덤으로 가져옴. 보여주고싶은 이미지는 64x64크기
-            'name' : '진은성',
-            'birthday' : '910501',
-            'gender' : '남자',
-            'job' : 'Programmer'
-          },
-          {
-            'id' : 2,
-            'image' : 'https://placeimg.com/64/64/2', // placeimg 사이트는 이미지를 랜덤으로 가져옴. 보여주고싶은 이미지는 64x64크기
-            'name' : '진은성2',
-            'birthday' : '920501',
-            'gender' : '남자',
-            'job' : 'Programmer'
-          },
-          {
-            'id' : 3,
-            'image' : 'https://placeimg.com/64/64/3', // placeimg 사이트는 이미지를 랜덤으로 가져옴. 보여주고싶은 이미지는 64x64크기
-            'name' : '진은성3',
-            'birthday' : '930501',
-            'gender' : '남자',
-            'job' : 'Programmer'
-          }
-    ]);
+    connection.query(
+      "SELECT * FROM MyInfo",
+      (err, rows, fields) => {
+        res.send(rows);
+      }
+    );
 });
 
 app.listen(port, () => console.log(`Listenin on port ${port}`));
--- management/src/App.js
+++ management/src/App.js
@@ -8,6 +8,7 @@ import TableRow from '@material-ui/core/TableRow';
 import TableCell from '@material-ui/core/TableCell';
 import {withStyles} from '@material-ui/core/styles';
 import Paper from '@material-ui/core/Paper';
+import CircularProgress from '@material-ui/core/CircularProgress';
 
 const styles = theme => ({
   root: {
@@ -17,26 +18,35 @@ const styles = theme => ({
   },
   table: {
     minWidth: 1080
+  },
+  progress: {
+    margin: theme.spacing.unit * 2
   }
 })
-
 
 class App extends Component {
 
   state = {
-    customers: ""
+    customers: "",
+    completed: 0
   }
 
   componentDidMount() {
+    this.timer = setInterval(this.progress, 20);
     this.callApi()
-      .then(res => this.setState({customers: res}))
-      .catch(err => console.log(err));
+    .then(res => this.setState({customers: res}))
+    .catch(err => console.log(err));
   }
 
   callApi = async () => {
     const response = await fetch('/api/customers');
     const body = await response.json();
     return body;
+  }
+
+  progress = () => {
+    const {completed} = this.state;
+    this.setState({completed: completed >= 100 ? 0 : completed +1});
   }
 
   render() {
@@ -68,7 +78,12 @@ class App extends Component {
                     job={c.job}
                   />
                 )
-              }) : ""
+              }) : 
+              <TableRow>
+                <TableCell colSpan="6" align="center">
+                  <CircularProgress className={classes.grogress} variant="determinate" value={this.state.completed} />
+                </TableCell>
+              </TableRow>
             }
           </TableBody>
         </Table>
